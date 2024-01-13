d614fd04a0397f084dc7db0d0223ce4ec0ff7d16
Fabi <fuerfabian@gmail.com>
2024-01-13 10:36:05 +0100

    feat: testing branch for distributed loops

---
+++ distrib_prototype.jl
@@ -0,0 +1,36 @@
+using SharedArrays
+using Distributed
+using ParallelUtilities
+
+function loop(n=10)
+    a = zeros(n)
+    for i = 1:n
+        sleep(1)
+        a[i] = i
+    end
+    println("done")
+    return a
+end
+"parallel loop: 1"
+function ploop1(n = 10)
+    pids = ParallelUtilities.workers_myhost()
+    pids = [1; 2; 3 ; 4]
+    a = SharedArray{Int}((n,), pids = pids)
+
+    @sync @distributed for i = 1:n
+        sleep(1)
+        a[i] = i
+    end
+    println("done")
+end
+"parallel loop: 2"
+function ploop2(n = 10)
+    pids = ParallelUtilities.workers_myhost()
+    a = SharedArray{Int}((n,), pids = pids)
+
+    for i = 1:n
+        sleep(1)
+        a[i] = i
+    end
+    println("done")
+end
