7627a9a75052f199ba5bcbf3c5fb7e9c78631a8b
Fabi <your@mail.com>
2024-01-18 17:44:04 +0100

    feat: prototype for distributed computing

--- distrib_prototype.jl
+++ distrib_prototype.jl
@@ -1,27 +1,34 @@
+using ParallelUtilities
 using SharedArrays
 using Distributed
 using ParallelUtilities
+using BenchmarkTools
+
+
+sleeptime=0.001
+
+
 
 function loop(n=10)
     a = zeros(n)
     for i = 1:n
-        sleep(1)
+        sleep(sleeptime)
         a[i] = i
     end
-    println("done")
+    #println("done")
     return a
 end
 "parallel loop: 1"
 function ploop1(n = 10)
     pids = ParallelUtilities.workers_myhost()
-    pids = [1; 2; 3 ; 4]
+    #pids = [1; 2; 3 ; 4]
     a = SharedArray{Int}((n,), pids = pids)
 
     @sync @distributed for i = 1:n
-        sleep(1)
+        sleep(sleeptime)
         a[i] = i
     end
-    println("done")
+    #println("done")
 end
 "parallel loop: 2"
 function ploop2(n = 10)
@@ -29,8 +36,19 @@ function ploop2(n = 10)
     a = SharedArray{Int}((n,), pids = pids)
 
     for i = 1:n
-        sleep(1)
+        sleep(sleeptime)
         a[i] = i
     end
     println("done")
 end
+@benchmark loop()
+
+@benchmark ploop1()
+
+addprocs(10)
+
+@everywhere using ParallelUtilities
+@everywhere using SharedArrays
+@everywhere using Distributed
+
+@benchmark ploop1()
