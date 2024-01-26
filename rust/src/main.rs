a553a32ab62ff0ebc32e91a106933509fa4c10ef
Fabi <your@mail.com>
2024-01-26 18:33:42 +0100

    feat: add test for CodeType

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,38 +1,36 @@
 const COLUMNS: usize = 4;
 const COLORS: u8 = 6;
 
+#[derive(Copy, Clone)]
 struct CodeType {
     entries : [u8; COLUMNS],
 }
 impl CodeType{
-    fn copy(self) -> [u8; COLUMNS]{
-        return self.entries
-    }
-    fn grade(self : CodeType, solution_in : CodeType) -> ResultType{
-        let mut line = self.copy();
-        let mut solution = solution_in.copy();
+    fn grade(self : CodeType, solution : CodeType) -> ResultType{
+        let mut line_bool = [false; COLUMNS];
+        let mut solution_bool =  [false; COLUMNS];
         let mut positions : u8 = 0;
         let mut colors : u8 = 0;
     
         for i in 0..COLUMNS {
-            if line[i] == solution[i]{
+            if self.entries[i] == solution.entries[i]{
                 positions += 1;
-                line[i] = 0;
-                solution[i] = 0;
+                line_bool[i] = true;
+                solution_bool[i] = true;
             }
         }
     
         for i in 0..COLUMNS{
-            if line[i] == 0{
+            if line_bool[i]{
                 continue;
             }
             for j in 0..COLUMNS{
-                if solution[j] == 0{
+                if solution_bool[j]{
                     continue;
                 }
-                else if line[i] == solution[j] {
-                    line[i] = 0;
-                    solution[j] = 0;
+                else if self.entries[i] == solution.entries[j] {
+                    line_bool[i] = true;
+                    solution_bool[j] = true;
                     colors += 1;
                     break
                 }
@@ -42,11 +40,13 @@ impl CodeType{
     }
 }
 
+#[derive(Copy, Clone, PartialEq, Debug)]
 struct ResultType {
     positions : u8,
     colors : u8,
 }
 
+#[derive(Copy, Clone)]
 struct LineType{
     code: CodeType,
     result: ResultType,
@@ -62,4 +62,30 @@ impl LineType {
 
 fn main() {
     println!("Hello, world!");
+}
+
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_CodeType(){
+        fn get_ct(a: u8, b: u8, c: u8, ref_number: u8) -> CodeType {
+            let mut line= [ref_number; COLUMNS];
+            line[0] = a;
+            line[1] = b;
+            line[2] = c;
+            return CodeType{entries: line};
+        }
+        let a = get_ct(1,2,2,0);
+        let b = get_ct(1,3,3,4);
+        assert_eq!(b.grade(a), ResultType{positions:1,colors:0});
+        let c = get_ct(9, 9, 9, 9);
+        assert_eq!(c.grade(a), ResultType{positions: 0, colors: 0});
+        let d = get_ct(2,1,3,4);
+        assert_eq!(a.grade(d), ResultType{positions:0, colors:2});
+    }
+    
 }
